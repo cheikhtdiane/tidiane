932ef19664afddf450746b0b510863a8751bd19e
cheikhtdiane <cheikhtidiane.sarr7@unchk.edu.sn>
2026-02-09 22:36:13 +0000

    Add files via upload

---
+++ exo.py
@@ -0,0 +1,43 @@
+import streamlit as st
+with st.sidebar:
+    st.image("Sarr.jpg",width=500)
+    st.header("Profil")
+    st.write("Technicien supérieur en géomatique avec des connaissances en base de SIG, cartographie numérique et gestion des données géographiques. Intéressé par l'analyse spatiale et la création de cartes thématiques.Sérieux,motivé et prêt a travailler avec l'équipe")
+    st.write("Tel: 78-842-14-95")
+    st.write(" Email: sarrchtidiane256@gmail.com")
+    st.write("Adresse: Hann Mariste")
+st.header("CHEIKH TDIANE SARR")
+st.header("Educations")
+st.write("BTS: Géomatique 2027/2028 CEDT le G-15, Dakar/Colobane")
+st.write("BAC 2023/2024 Lycée Léopold Sédar Senghor de Joal-Fadiouth,Sénégal")
+st.write("BFEM 2020/2021 Collège de la Petite Côte de Joal-Fadiouth")
+
+st.header("Compétences")
+st.write("Réalisation des carte sur Autocad et QGis")
+st.write("Bonne maîtrise de langage SQL")
+st.write("Analyser et partager des données")
+st.write("Dessiner un plan avec l'utilisation du logiciel Autocad")
+
+st.header("Expériences")
+st.write("Collecte et traitement des données géographiques")
+st.write("Codage avec toutes les langages de programmation")
+st.write("Réalisation de projet SIG")
+
+st.header("Projets Académiques")
+st.write("Production de carte a l'aide d'une décision")
+st.write("Réalisation de carte thématique d'une commune")
+st.write("Création d'un application pour collecter des données géographique")
+st.write("Mise en oeuvre des plans d'architecture")
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
